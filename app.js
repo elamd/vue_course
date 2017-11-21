da50a8638475df54a2d3f44c9c7de366563f9ffc
Elam <elam.daly@gmail.com>
2017-11-21 18:25:44 -0500

    progress

--- app.js
+++ app.js
@@ -4,26 +4,29 @@ new Vue({
 
   data: {
     event: {
-      eventDate: 'Event Date',
-      eventTitle:'Event Title',
-      eventDescription:'Event Description',
-      signUpText:'Add your name to the guest list for <em>exclusive</em> offers.'
+      eventDate: 'August 14th - 18th',
+      eventTitle:'Summer Festival',
+      eventDescription:"It's back!",
+      signUpText:'Add your name to the guest list for <em>exclusive</em> offers.',
+      
     },
     newNameText: '',
     guestName:['James','Chris','Sam'],
-    formSubmitClass: '',
     appStyles: {
       marginTop: '25px',
-    }
+    },
+    eventCapacity: 25,
+    eventCapacityPercentage: 0    
   },
 
   methods: {
     formSubmitted: function() {
       console.log(this.newNameText);
-      if(this.newNameText.length > 0) {
+      if(this.newNameText.length > 0 && this.eventCapacityPercentage < 100) {
         this.guestName.push(this.newNameText);
         this.newNameText = '';
-        this.formSubmitClass = 'submitted';
+        this.eventCapacityPercentage = this.guestName.length / this.eventCapacity * 100;
+        console.log(this.eventCapacityPercentage);
       }
     }
   }
